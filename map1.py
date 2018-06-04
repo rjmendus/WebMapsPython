61846feacdcb6c5f47455c5dc243041142301aab
Rajat Mendus <jaicmendus@gmail.com>
2018-06-04 12:13:52 +0530

    Add a single marker

--- map1.py
+++ map1.py
@@ -2,4 +2,9 @@ import folium
 
 map = folium.Map([9.9632, 76.2532], zoom_start=6, tiles="Mapbox Bright")
 
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[9.6, 76.4], popup="Hello", icon=folium.Icon(color="red")))
+
+map.add_child(fg)
+
 map.save("Map1.html")
