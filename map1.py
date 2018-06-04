e1d9586d7fa543bcf68f9b69ba63efe1604ae310
Rajat Mendus <jaicmendus@gmail.com>
2018-06-04 12:25:23 +0530

    Add multiple markers manually

--- map1.py
+++ map1.py
@@ -3,7 +3,8 @@ import folium
 map = folium.Map([9.9632, 76.2532], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[9.6, 76.4], popup="Hello", icon=folium.Icon(color="red")))
+for coordinates in [[9.6, 76.4],[8.6, 75.4]]:
+	fg.add_child(folium.Marker(location=coordinates, popup="Hello", icon=folium.Icon(color="red")))
 
 map.add_child(fg)
 
