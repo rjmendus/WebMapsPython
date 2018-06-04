37ad2ae96da2ae85d3886a1d92226b8fa31f43d1
Rajat Mendus <jaicmendus@gmail.com>
2018-06-04 15:37:06 +0530

    Add layer control

--- map1.py
+++ map1.py
@@ -20,21 +20,26 @@ def get_elevation_color(elevation):
 
 # Defining the map and feature group
 map = folium.Map([38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
+fgp = folium.FeatureGroup(name="Population Density")
 
 # Adding the markers from the list
 for lt, ln, el in zip(lat, lon, elev):
-	fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el) + " m",
+	fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el) + " m",
 		fill_color = get_elevation_color(el), color= "black", fill = True, fill_opacity=0.7))
 
 # Opening the GeoJson file
 geo_json = open("world.json", "r", encoding="utf-8-sig")
 
 # Adding the polygons to map
-fg.add_child(folium.GeoJson(data=geo_json.read(),
+fgp.add_child(folium.GeoJson(data=geo_json.read(),
 	style_function = lambda x: {"fillColor":"green" if x["properties"]["POP2005"] < 10000000 
 	else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 else "red"}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+# Adding layer control
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
