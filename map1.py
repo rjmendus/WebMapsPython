3332edd40e668526089a4221f5cbfb7f1ff361f8
Rajat Mendus <jaicmendus@gmail.com>
2018-06-04 13:37:02 +0530

    Fetch volcano coordinates from the data file.

--- map1.py
+++ map1.py
@@ -1,10 +1,21 @@
 import folium
+import pandas
 
-map = folium.Map([9.9632, 76.2532], zoom_start=6, tiles="Mapbox Bright")
+# Loading the data from the file Volcanoes.txt
+data = pandas.read_csv("Volcanoes.txt")
 
+# Getting the coordinates to a list
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+# Defining the map and feature group
+map = folium.Map([38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 fg = folium.FeatureGroup(name="My Map")
-for coordinates in [[9.6, 76.4],[8.6, 75.4]]:
-	fg.add_child(folium.Marker(location=coordinates, popup="Hello", icon=folium.Icon(color="red")))
+
+# Adding the markers from the list
+for lt, ln, el in zip(lat, lon, elev):
+	fg.add_child(folium.Marker(location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color="red")))
 
 map.add_child(fg)
 
